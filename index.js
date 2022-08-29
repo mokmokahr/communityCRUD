93042ed6c885749f94c406aa97f234a3ba9b386e
mokmokahr <jbmok.workspace@gmail.com>
2022-08-29 18:10:12 +0900

    FEAT: Make update function

--- index.js
+++ index.js
@@ -25,22 +25,43 @@ const Results = sequelize.define('Results', {
     await Results.sync();
 })();
 
-app.use(express.json()); 
+app.use(express.json());
 app.use(express.urlencoded( {extended : true } ));
 
 app.get('/', async function (req, res) {  //main page
     const results = await Results.findAll({raw: true});
-    res.render("home.ejs",{results: results});
+    res.render("home",{results: results});
 });
 
 app.get('/create', function(req,res){  //form page
     res.render("create");
 });
 
+app.get('/update/:id', async function (req, res){
+    const result = await Results.findOne({
+        where: { id: Number.parseInt(req.params.id, 10) }
+    });
+    if(result !== null) {
+        res.render("update",{result:result});
+    } else {
+        res.sendStatus(404);
+    }
+});
+
+app.post('/update/:id',async function(req,res){
+    console.log(req.body, req.params);
+    await Results.update({title:req.body.title, main:req.body.main}, {
+        where:{
+            id: Number.parseInt(req.params.id,10)
+        }
+    });
+    res.redirect("/");
+});
+
+
 app.post('/sendResult', async function(req,res){
     const content = req.body;
     resultCreate = await Results.create({title: content.title, main:content.main});
-    console.log(resultCreate.title);
     res.redirect('/');
 });
 
@@ -50,18 +71,9 @@ app.post('/delete/:id',async function(req,res){
         where:{
             id: id
         }
-    })
+    });
     res.redirect('/');
 });
-
-app.post('/update/:id',async function(req,res){
-    const {id} = req.params;
-    await Results.update({
-        where:{
-            id:id
-        }
-    })
-})
 
 app.use(express.static("views"));
 app.listen(3000);
